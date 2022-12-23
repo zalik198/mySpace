8f48ad9734baeb38f6838fb27173473a97c04867
zalik198 <zshomakhov@mail.ru>
2022-12-23 10:30:51 +0300

    Initial Commit

---
+++ mySpace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mySpace
+//
+//  Created by Shom on 23.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
